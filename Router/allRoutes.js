2691e4307515f83d4b2e6be70a1f3b22cb781346
sekou78 <s.cisse78@gmail.com>
2024-11-02 20:15:44 +0100

    Mise en place de la page restaurant

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,15 +2,20 @@ import Route from "./Route.js";
 
 //Définir ici nos routes
 export const allRoutes = [
-  new Route("/", "Accueil-visiteur", "/pages/accueil-visiteur.html"),
+  new Route("/", "Accueil-visiteur", "/pages/accueil/accueil-visiteur.html"),
   new Route(
     "/accueil-connectee",
     "Accueil-connectee",
-    "/pages/accueil-connectee.html"
+    "/pages/accueil/accueil-connectee.html"
   ),
   new Route("/contact", "Formulaire-contact", "/pages/contact.html"),
   new Route("/avis", "Votre-avis", "/pages/avis.html"),
-  new Route("/services", "Les-Services", "/pages/services.html"),
+  new Route("/services", "Les-Services", "/pages/services/services.html"),
+  new Route(
+    "/restaurant",
+    "La-restauration",
+    "/pages/services/restaurant.html"
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
